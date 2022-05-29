96ff6d9009918af35e5a530e25b799fc6f7c76c9
ulalala <reyoula@gmail.com>
2022-05-30 03:26:48 +0900

    bookmark model

--- app/src/main/java/org/techtown/stockking/adapter/BookmarkListAdapter.kt
+++ app/src/main/java/org/techtown/stockking/adapter/BookmarkListAdapter.kt
@@ -2,12 +2,14 @@ package org.techtown.stockking.adapter
 
 
 import android.R.attr.data
+import android.graphics.Color
 import android.graphics.drawable.PictureDrawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.caverock.androidsvg.SVG
+import org.techtown.stockking.R
 import org.techtown.stockking.databinding.BookmarkRecyclerviewBinding
 import org.techtown.stockking.fragment.BookmarkFragment
 import org.techtown.stockking.model.BookmarkListModel
@@ -38,23 +40,23 @@ class BookmarkListAdapter(
 
         val ticker = bookmarkList[position].symbol
         binding.itemTicker.text = ticker
-        binding.itemConame.text = bookmarkList[position].name_kr
+        binding.itemConame.text = bookmarkList[position].name
         binding.itemClose.text =  bookmarkList[position].close
 
-//        if(bookmarkList[position].change_percent.substring(0,1)=="-"){
-//            binding.itemChangePercent.setTextColor(Color.BLUE)
-//            binding.itemChangeValue.setTextColor(Color.BLUE)
-//            binding.itemChangePercent.text = context.resources.getString(R.string.down)+bookmarkList[position].change_percent.substring(1) + context.resources.getString(
-//                R.string.percent)
-//            binding.itemChangeValue.text = bookmarkList[position].change_value + context.resources.getString(
-//                R.string.dollar)
-//        }else{
-//            binding.itemChangePercent.setTextColor(Color.RED)
-//            binding.itemChangeValue.setTextColor(Color.RED)
-//            binding.itemChangePercent.text = context.resources.getString(R.string.up)+bookmarkList[position].change_percent.substring(1) + context.resources.getString(
-//                R.string.percent)
-//            binding.itemChangeValue.text = bookmarkList[position].change_value + context.resources.getString(R.string.dollar)
-//        }
+        if(bookmarkList[position].change_percent.substring(0,1)=="-"){
+            binding.itemChangePercent.setTextColor(Color.BLUE)
+            binding.itemChangeValue.setTextColor(Color.BLUE)
+            binding.itemChangePercent.text = context.resources.getString(R.string.down)+bookmarkList[position].change_percent.substring(1) + context.resources.getString(
+                R.string.percent)
+            binding.itemChangeValue.text = bookmarkList[position].change_value + context.resources.getString(
+                R.string.dollar)
+        }else{
+            binding.itemChangePercent.setTextColor(Color.RED)
+            binding.itemChangeValue.setTextColor(Color.RED)
+            binding.itemChangePercent.text = context.resources.getString(R.string.up)+bookmarkList[position].change_percent.substring(1) + context.resources.getString(
+                R.string.percent)
+            binding.itemChangeValue.text = bookmarkList[position].change_value + context.resources.getString(R.string.dollar)
+        }
 
         //SVG string content
         val svgString = bookmarkList[position].img
